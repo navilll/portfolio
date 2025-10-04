44d1e28881b906c44a06f9aeedca39f7675f1261
navilll <navilfaisal00@gmail.com>
2025-10-04 15:06:38 +0530

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,8 @@ const About = () => {
     <div className="py-10 text-white bg-[#232325] h-auto" id='about'>
         <div className="flex sm:flex-row flex-col-reverse items-center md:gap-6 px-10 max-w-6xl mx-auto m-12">
             <div>
-                <div className="w-[350px] h-full mt-4">
-                    <img src={about} className='object-cover rounded-xl h-[300px] filter brigtness-50' />
+                <div className="w-[350px] h-full">
+                    <img src={about} className='object-cover rounded-xl h-[300px] filter brigtness-50 mt-4' />
                 </div>
             </div>
 
@@ -26,22 +26,22 @@ const About = () => {
                     </div>
                 </div>
 
-                <div className="flex mt-10 items-stretch gap-4 md-2">
-                    <div className="flex-1 bg-[#333333] p-5 rounded-lg flex flex-col items-center justify-center">
+                <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 max-w-6xl mx-auto px-4 mt-10">
+                    <div className="bg-[#333333] p-5 rounded-lg flex flex-col items-center justify-center">
                         <h3 className="md:text-4xl text-2xl font-semibold text-white">
                         15<span>+</span>
                         </h3>
                         <p className="md:text-base text-sm text-center">Projects</p>
                     </div>
-                    
-                    <div className="flex-1 bg-[#333333] p-5 rounded-lg flex flex-col items-center justify-center">
+
+                    <div className="bg-[#333333] p-5 rounded-lg flex flex-col items-center justify-center">
                         <h3 className="md:text-4xl text-2xl font-semibold text-white">
                         3<span>+</span>
                         </h3>
                         <p className="md:text-base text-sm text-center">Years of Experience</p>
                     </div>
-                    
-                    <div className="flex-1 bg-[#333333] p-5 rounded-lg flex flex-col items-center justify-center">
+
+                    <div className="bg-[#333333] p-5 rounded-lg flex flex-col items-center justify-center">
                         <h3 className="md:text-4xl text-2xl font-semibold text-white">
                         10<span>+</span>
                         </h3>
